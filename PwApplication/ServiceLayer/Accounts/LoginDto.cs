0967ca30eef8fe21b093ab13cce5386942ea19e7
Katherine Burova <katherineburova@gmail.com>
2019-03-29 23:56:02 +0300

    Data annotations updated

--- PwApplication/ServiceLayer/Accounts/LoginDto.cs
+++ PwApplication/ServiceLayer/Accounts/LoginDto.cs
@@ -5,10 +5,8 @@ namespace ServiceLayer.Accounts
     public class LoginDto
     {
         [Required]
-        [Display(Name = "Email:")]
         public string Email { get; set; }
         [Required]
-        [Display(Name = "Password:")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
